cd78d4d821e9f729d0594d338eb207d664d594e4
ankush0511 <ankushchaudhary051104@gmail.com>
2025-04-30 11:11:43 +0530

    1 exp run

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -6,7 +6,8 @@ import pandas as pd
 from sklearn import metrics
 from sklearn import tree
 from dvclive import Live
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 
 def evaluate(model, X, y, split, live, save_path):
